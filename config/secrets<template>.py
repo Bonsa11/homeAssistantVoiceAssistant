19cee30ebd62456e33425f9b2b18398a71a9af66
Bonsa11 <sambodza@hotmail.co.uk>
2022-05-31 19:01:14 +0100

    added secrets_template file

---
+++ config/secrets<template>.py
@@ -0,0 +1,4 @@
+# add your info below and remove the <template> from the file name so it's secrets.py
+
+token =  '' # long lived access token for HA
+HA_url = '' # address of home assistant instance
